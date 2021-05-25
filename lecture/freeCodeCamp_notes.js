c3745de2ff9f011a4a9cf39293d39b423c9c3373
engrCoy <engr.nicolassebastianmendoza@gmail.com>
2021-05-26 02:55:31 +0800

    javascript practice
    recursion, spread operators, and deconstructing

--- lecture/freeCodeCamp_notes.js
+++ lecture/freeCodeCamp_notes.js
@@ -10,27 +10,52 @@
 
 
 // check var and let with changing variables
-var ctr = 0;
-function inc() {
-    for(var i = 0; i < 10; i++) {
-        ctr += i;
+// var ctr = 0;
+// function inc() {
+//     for(var i = 0; i < 10; i++) {
+//         ctr += i;
         
-    }
-    return ctr;
-}
+//     }
+//     return ctr;
+// }
 
 // console.log("var i is =" + i); // error
-console.log("var ctr total is " + inc());
+// console.log("var ctr total is " + inc());
 
-let ctr2 = 0;
-function inc2() {
-    for(let j = 0; j < 10; j++) {
-        ctr2 += j;
+// let ctr2 = 0;
+// function inc2() {
+//     for(let j = 0; j < 10; j++) {
+//         ctr2 += j;
         
-    }
-    return ctr2;
-}
+//     }
+//     return ctr2;
+// }
 
 // console.log("let j is =" + j); // error
-console.log("let ctr2 total is " + inc());
+// console.log("let ctr2 total is " + inc());
 
+
+// spread operator function
+// let total = 0;
+// function numTotal([...number],mul) {
+//     for(let i = 0; i < number.length; i++) {
+//         total += number[i];
+//     }
+//     return total*mul;
+// }
+// console.log(numTotal([5,6,7,8,10,1,3],2));
+
+//try recursion with spread operators
+
+// function countdown(n) {
+//     return n < 1 ? [] : [n, ...countdown(n-1)];
+// }
+// console.log(countdown(10));
+
+
+//try recursion with spread operator with max, and min
+function countdown(max, min) {
+     return max <= min ? [min]:[max, ...countdown(max-1, min)];
+}
+
+console.log(countdown(10,5));
