d87ae1cddc788669026c0daf5982b75c62b7ea58
engrCoy <engr.nicolassebastianmendoza@gmail.com>
2021-05-27 22:04:56 +0800

    practice object manipulation
    constructor function, getter, and setter functions

--- lecture/freeCodeCamp_notes.js
+++ lecture/freeCodeCamp_notes.js
@@ -62,36 +62,121 @@
 
 //create sample objects for practice on property manipulation;
 
-let myInformation = {
-    name: "Nicolas Sebastian R. Mendoza",
-    address: "Capas, Tarlac",
-    hobbies: ["programming", "music", "watching movies"],
-    age: 25,
-    birthday: '06/19/1996',
-    pets: [{
-            petName: "Suri",
-            type: "dog",
-            breed: "Shih-Tzu",
-            food: ["chicken liver","chicken adobo","dog food"]
-        },
-        {
-            petName: "Sid",
-            type: "dog",
-            breed: "Belgian Malinois",
-            food: ["corned-beef","dog food"]
-        },
-        {
-            petName: "Link",
-            type: "cat",
-            breed: "Persian",
-            food: ["cat food"]
-        },
-        {
-            petName: "Euruce",
-            type: "cat",
-            breed: "Puskal",
-            food: ["cat food"]
-        }
+// let myInformation = {
+//     name: "Nicolas Sebastian R. Mendoza",
+//     address: "Capas, Tarlac",
+//     hobbies: ["programming", "music", "watching movies"],
+//     age: 25,
+//     birthday: '06/19/1996',
+//     pets: [{
+//             petName: "Suri",
+//             type: "dog",
+//             breed: "Shih-Tzu",
+//             food: ["chicken liver","chicken adobo","dog food"]
+//         },
+//         {
+//             petName: "Sid",
+//             type: "dog",
+//             breed: "Belgian Malinois",
+//             food: ["corned-beef","dog food"]
+//         },
+//         {
+//             petName: "Link",
+//             type: "cat",
+//             breed: "Persian",
+//             food: ["cat food"]
+//         },
+//         {
+//             petName: "Euruce",
+//             type: "cat",
+//             breed: "Puskal",
+//             food: ["cat food"]
+//         }
         
-    ]
-}
+//     ]
+// }
+
+// factory function
+// function createCircle(radius) {
+//     return {
+//         radius,
+//         draw() {
+//             console.log('draw');
+//         }
+//     }
+// }
+
+//constructor function
+// const myCircle = createCircle(1); 
+// console.log(myCircle);
+// //constructor function
+
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function() {
+//         console.log('draw');
+//     }
+// }
+
+// const circle = new Circle(2);
+// console.log(circle);
+// console.log();
+// console.log(Circle);  
+
+
+// class GotChar {
+//     constructor (firstName, lastName) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+// }
+
+// const charNew = new GotChar('Ned','Stark');
+// console.log(charNew);
+
+// setter and getter function
+
+// class Book {
+//     constructor(author) {
+//       this._author = author;
+//     }
+//     // getter
+//     get writer() {
+//       return this._author;
+//     }
+//     // setter
+//     set writer(updatedAuthor) {
+//       this._author = updatedAuthor;
+//     }
+//   }
+//   const novel = new Book('anonymous');
+//   console.log(novel.writer);
+//   novel.writer = 'newAuthor';
+//   console.log(novel.writer);
+
+
+//   console.log(novel);
+//   console.log(novel.author);
+//   console.log(novel._author);
+
+  class Names {
+      constructor(firstName, lastName) {
+          this._firstName = firstName;
+          this._lastName = lastName;
+      }
+      get givenName() {
+          return `${this._firstName} ${this._lastName} is my Name`
+      }
+      set changeName(name) {
+          this._firstName = name.split(' ')[0];
+          this._lastName = name.split(' ')[1]
+      }
+  }
+
+  const newName = new Names('Nicolas','Mendoza');
+  console.log(newName);
+  let wholeName = newName.givenName;
+  console.log(wholeName);
+  newName.changeName = "Dolores Mendoza";
+  console.log(newName.givenName);
+
